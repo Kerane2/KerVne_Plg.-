65994d30a2421ff1eeddda324daf7904765b244a
*Kervne_Plg* <112810924+Kerane2@users.noreply.github.com>
2025-05-02 00:45:20 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  author: '‎Kerane Plg.',
+  botName: " KerVneBot.",
+  botOwner: 'Kerane Plg', // Your name
+  ownerNumber: '241066435809', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
